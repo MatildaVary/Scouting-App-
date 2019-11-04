104c7d96e75c13c1570ce864f941531194a1577e
Matilda Vary <matildavary@Matildas-MacBook-Pro.local>
2019-11-04 07:27:10 -0800

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Scouting App
+//
+//  Created by Matilda Vary on 10/23/19.
+//  Copyright © 2019 Matilda Vary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func GameHatchStepper(_ sender: UIStepper) {
+    }
+}
+
